519fe73cb426558c7e4cb743544af26d2c847549
Jamie <Jamie@Jamies-MacBook-Air.local>
2022-04-04 21:39:39 -0400

    added component folder & clock.js file

---
+++ src/components/Clock.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import {Component} from 'react';
+
+
+// just class def & you shuld hve only 1 render call
+
+class Clock extends Component {
+    constructor(props) {
+      super(props);
+      this.state = {date: new Date()};
+    }
+  
+    componentDidMount() {
+      this.timerID = setInterval(
+        () => this.tick(),1000
+      );
+    }
+  
+    componentWillUnmount() {
+      clearInterval(this.timerID);
+    }
+  
+    tick() {
+      this.setState({
+        date: new Date()
+      });
+    }
+  
+    render() {
+      return (
+        <div>
+          <h1>Hello, world!</h1>
+          <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+        </div>
+      );
+    }
+  }
+  
+  
+
+
+  export default Clock; //export them from the file 
